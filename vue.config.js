9b3918f633e01722e96244a204ae47c156b937d0
Raghavendra Saralaya <raghavendra.saralaya@gmail.com>
2023-02-07 00:19:37 +0530

    updates

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,27 @@
-const { defineConfig } = require('@vue/cli-service')
+const webpack = require("webpack");
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+});
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: ["buffer", "Buffer"],
+      }),
+    ],
+    resolve: {
+      fallback: {
+        crypto: false,
+        fs: false,
+        assert: false,
+        process: false,
+        util: false,
+        path: false,
+        stream: false,
+      },
+    },
+  },
+});
